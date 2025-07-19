0b98339111c2e70cdc73863db063803c495485f0
Khaled <khaled.ashraf.pro@gmail.com>
2025-07-19 07:12:30 +0300

    Update file paths in config and logging functions for consistency; remove unused JSON utility

--- utils/app_setup.py
+++ utils/app_setup.py
@@ -2,6 +2,7 @@ import logging
 import json
 import os
 
+
 def setup_logging():
     """
     Configures logging for the entire application.
@@ -12,6 +13,7 @@ def setup_logging():
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
+
 def load_config(config_path='config.json'):
     """
     Loads the JSON configuration file.
@@ -19,7 +21,7 @@ def load_config(config_path='config.json'):
     if not os.path.exists(config_path):
         logging.error(f"Configuration file not found at {config_path}")
         raise FileNotFoundError(f"Configuration file not found at {config_path}")
-    
+
     with open(config_path, 'r') as f:
         config = json.load(f)
     logging.info("Configuration loaded successfully.")
--- utils/scraping_utils.py
+++ utils/scraping_utils.py
@@ -1,10 +1,16 @@
-def log_failed_url(url, output_file='failed_urls.txt'):
+from utils.app_setup import load_config
+
+
+config = load_config()
+
+
+def log_failed_url(url, output_file=config['failed_urls_file_path']):
     """Appends a failed URL to the designated log file."""
     with open(output_file, 'a', encoding='utf-8') as f:
         f.write(url + '\n')
 
 
-def log_skipped_url(url):
+def log_skipped_url(url, output_file=config['skipped_urls_file_path']):
     """Appends a skipped URL to the designated log file."""
-    with open('skipped_urls.txt', 'a', encoding='utf-8') as f:
+    with open(output_file, 'a', encoding='utf-8') as f:
         f.write(url + '\n')
